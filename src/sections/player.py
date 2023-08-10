d9c300ed93741a09d54265861429788f187e2ac1
Json Jimenez <jeison@jeison.com>
2023-08-10 18:11:57 -0500

    update fox

--- src/sections/player.py
+++ src/sections/player.py
@@ -1,6 +1,7 @@
 import json
 import vlc;
 from src.services.songs_service import SongService;
+from src.services.network_service import NetworkService;
 
 class Player():
   songService = SongService();
@@ -10,29 +11,36 @@ class Player():
     self.logger = logger;
   
   def play(self):
-    responseSong = self.songService.getNextSong();
-    if responseSong.status_code == 200:
-      pdvResponse = json.loads(responseSong.text);
-      self.logger.info("Punto de venta cargado");
-    else:
-      self.logger.critical("Punto de venta fallido");
-      return False;
+    has_internet = NetworkService.check_internet_connection();
+    if has_internet:
+      responseSong = self.songService.getNextSong();
+      if responseSong.status_code == 200:
+        pdvResponse = json.loads(responseSong.text);
+        self.logger.info("Punto de venta cargado");
+      else:
+        self.logger.critical("Punto de venta fallido");
+        return False;
 
-    nextSong = pdvResponse['payload'];
-    try:
-      media = vlc.Media(nextSong['song']['url']);
-      self.player.set_media(media);
-      self.player.play();
-      self.logger.info('Escuchando ' + nextSong['song']['name']);
-      self.songService.logSong(nextSong);
-    except Exception:
-      self.logger.critical('Error al reporducir la canción');
-      return False;
-    
-    while True:
-      state = self.player.get_state();
-      if state == vlc.State.Ended:
-        self.play();
+      nextSong = pdvResponse['payload'];
+      try:
+        media = vlc.Media(nextSong['song']['url']);
+        self.player.set_media(media);
+        self.player.play();
+        self.logger.info('Escuchando ' + nextSong['song']['name']);
+        self.songService.logSong(nextSong);
+      except Exception:
+        self.logger.critical('Error al reporducir la canción');
+        return False;
+      
+      while True:
+        state = self.player.get_state();
+        if state == vlc.State.Ended:
+          self.play();
+    else:
+      self.logger.info("No internet connection available.");
+      songdefault = self.playDefault();
+      if songdefault:
+            self.initApp();
 
   def playDefault(self):
     try:
